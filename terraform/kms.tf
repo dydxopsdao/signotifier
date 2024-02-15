c9c1c0371f7e8f9a03303fcb4df2fb2ea4622cbd
Piotr Święs <10480513+pswies@users.noreply.github.com>
2024-02-15 18:01:38 +0100

    Remove unnecessary KMS key parameter

--- terraform/kms.tf
+++ terraform/kms.tf
@@ -2,7 +2,6 @@ resource "aws_kms_key" "signing_key" {
   description              = "RSA 4096 key for message signing"
   customer_master_key_spec = "RSA_4096"
   key_usage                = "SIGN_VERIFY"
-  deletion_window_in_days  = 10
 }
 
 data "aws_kms_public_key" "signing_key" {
